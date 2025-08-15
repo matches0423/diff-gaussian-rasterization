7969acaaf51b0aa17a736890ba710580a7c5374a
陳翰霆 <b11015006@mail.ntust.edu.tw>
2025-08-16 02:25:41 +0800

    update build script

--- build.bat
+++ build.bat
@@ -2,14 +2,25 @@
 
 chcp 65001
 
-rmdir /s /q build
-mkdir build
+REM download torch(v2.0.0 w/ cuda 11.8)
+call curl -L -o torch.zip "https://download.pytorch.org/libtorch/cu118/libtorch-win-shared-with-deps-2.0.0%%%%2Bcu118.zip"
+    REM extract torch
+    rmdir /s /q third_party\libtorch
+    call tar -xf torch.zip -C third_party
+del torch.zip
 
+REM setup VS build environment
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+
+REM check nvcc
 call nvcc --version
 
-cmake -B build -D CMAKE_PREFIX_PATH=third_party/torch/ -D USE_CUDNN=1
+REM run cmake to generate project files
+rmdir /s /q build
+mkdir build
+call cmake -B build -D CMAKE_PREFIX_PATH="third_party\libtorch\" -D USE_CUDNN=1
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" ./build/CudaRasterizer.vcxproj -p:Configuration=Release
+REM compile
+call MSBuild build\CudaRasterizer.vcxproj -p:Configuration=Release
 
 pause
